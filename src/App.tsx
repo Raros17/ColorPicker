6202a66cf198783f1bdf2cd89873e1a3c9189d75
Raros17 <sookmeoyhj@gmail.com>
2024-07-30 16:25:16 +0900

    design: main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
-import './styles/global-style.css';
+import styled from 'styled-components';
 import React from 'react';
 
 function App() {
   return (
-    <div></div>
+    <MainSection>
+      <InputSection>
+        <Title>이미지 색상 넣기</Title>
+        <TextInput type="text"></TextInput>
+        <ImageBtn>버튼</ImageBtn>
+      </InputSection>
+    </MainSection>
   );
 }
 
 export default App;
+
+const ImageBtn = styled.button`
+  width: 100px;
+  height: 2rem;
+  color: #fff;
+  background-color: #272727;
+  border-radius: 20px;
+  cursor: pointer;
+`
+
+const InputSection = styled.section`
+  width: 80%;
+  background-color: #212121;
+  border-radius: 20px;
+  padding: 20px;
+`
+
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 700;
+  color: white;
+`
+
+const MainSection = styled.section`
+  width: 100%;
+  height: 100vh;
+  background-color: #444444;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const TextInput = styled.input`
+  width: 50%;
+  height: 1.5rem;
+  border-radius: 10px;
+`
