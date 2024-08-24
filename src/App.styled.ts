b8a5fb2739151703a0c8ea82b00a8e6675ac7d18
Raros17 <sookmeoyhj@gmail.com>
2024-08-24 22:19:16 +0900

    style: App.ts styled 컴포넌트 분리

---
+++ src/App.styled.ts
@@ -0,0 +1,94 @@
+
+import styled from 'styled-components';
+
+interface TextSectionProps {
+    colorData: string[];
+  }
+
+export const PlaceholderImage = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  background-color: #1b1a1a; 
+  margin-top: 20px;
+`
+
+export const StyledImage = styled.img`
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  object-fit: cover;
+  margin-top: 20px;
+`
+
+export const ColorDownBtn = styled.button`
+  height: 50px;
+  border-radius: 20px;
+  border: none;
+  transition: all 0.5s ease;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 800;
+  background-color: #262626;
+  color: #fff;
+  padding: 0 1rem;
+  font-family: 'TheJamsil5Bold';
+  &:hover {
+    background-color: #101010;
+  }
+  @media (max-width: 800px) {
+  width: 70px;
+  font-size: 15px;
+}
+`
+
+
+export const ImageGroup = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap; 
+  justify-content: center;
+  width: 100%;
+`
+
+
+export const TextSection = styled.section<TextSectionProps>`
+  width: 80%;
+  background: ${({ colorData }) =>
+    getBackgroundGradient(colorData, '#212121')};
+  border-radius: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+  color: white;
+  margin-bottom: 10px;
+  font-family : 'HSSanTokki20-Regular';
+`
+
+export const MainSection = styled.section<TextSectionProps>`
+  width: 100vw;
+  min-height: 100vh; 
+  background: ${({ colorData }) =>
+    getBackgroundGradient(colorData, '#444444')};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+export const getBackgroundGradient = (colorData: string[], fallbackColor: string) => {
+  if (colorData.length > 1) {
+    return `linear-gradient(to right, ${colorData[0]}, ${colorData[1]})`;
+  }
+  return fallbackColor;
+};
--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import ColorTheif from './component/colorTheif.tsx';
 import ColorModal from './component/colorModal.tsx';
 import ColorCodes from './ColorCode.tsx';
 import ImageUploader from './ImageUploader.tsx';
+import { 
+  PlaceholderImage, 
+  StyledImage, 
+  ColorDownBtn, 
+  ImageGroup, 
+  TextSection, 
+  Title, 
+  MainSection 
+} from './App.styled.ts'; 
 
-interface TextSectionProps {
-  colorData: string[];
-}
+
 
 function App() {  
   const [colorData, setColorData] = useState<string[]>([]);
@@ -69,91 +76,3 @@ function App() {
 }
 
 export default App;
-
-const PlaceholderImage = styled.div`
-  width: 300px;
-  height: 300px;
-  border-radius: 10px;
-  background-color: #1b1a1a; 
-  margin-top: 20px;
-`
-
-const StyledImage = styled.img`
-  width: 300px;
-  height: 300px;
-  border-radius: 10px;
-  object-fit: cover;
-  margin-top: 20px;
-`
-
-const ColorDownBtn = styled.button`
-  height: 50px;
-  border-radius: 20px;
-  border: none;
-  transition: all 0.5s ease;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 20px;
-  font-weight: 800;
-  background-color: #262626;
-  color: #fff;
-  padding: 0 1rem;
-  font-family: 'TheJamsil5Bold';
-  &:hover {
-    background-color: #101010;
-  }
-  @media (max-width: 800px) {
-  width: 70px;
-  font-size: 15px;
-}
-`
-
-
-const ImageGroup = styled.div`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap; 
-  justify-content: center;
-  width: 100%;
-`
-
-
-const TextSection = styled.section<TextSectionProps>`
-  width: 80%;
-  background: ${({ colorData }) =>
-    getBackgroundGradient(colorData, '#212121')};
-  border-radius: 20px;
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h1`
-  font-size: 20px;
-  font-weight: 700;
-  color: white;
-  margin-bottom: 10px;
-  font-family : 'HSSanTokki20-Regular';
-`
-
-const MainSection = styled.section<TextSectionProps>`
-  width: 100vw;
-  min-height: 100vh; 
-  background: ${({ colorData }) =>
-    getBackgroundGradient(colorData, '#444444')};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`;
-
-const getBackgroundGradient = (colorData: string[], fallbackColor: string) => {
-  if (colorData.length > 1) {
-    return `linear-gradient(to right, ${colorData[0]}, ${colorData[1]})`;
-  }
-  return fallbackColor;
-};
