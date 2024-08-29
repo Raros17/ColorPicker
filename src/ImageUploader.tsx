490bce254d56ac0dc8fa6efeb3ea885c4cea1d2b
Raros17 <sookmeoyhj@gmail.com>
2024-08-29 10:45:43 +0900

    style: ImgUploader styled 분리

--- src/ImageUploader.tsx
+++ src/ImageUploader.tsx
@@ -1,5 +1,5 @@
 import React, {useRef} from 'react';
-import styled from 'styled-components';
+import { ImageBtn, InputSection, InputTypeSection, TextDeleteBtn, TextInput } from './ImageUploader.styled.ts';
 
 interface ImageUploaderProps {
   inputRef: React.RefObject<HTMLInputElement>;
@@ -64,64 +64,3 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ inputRef, handleProxyUrl,
 
 export default ImageUploader;
 
-
-
-const TextInput = styled.input`
-  width: 100%;
-  height: 2rem;
-  border-radius: 10px;
-  margin-right: 10px;
-  font-size: 15px;
-  padding-left: 1rem;
-  color: #888;
-  font-family: 'TheJamsil5Bold';
-  outline: none;
-  border  :none ;
-`
-
-const TextDeleteBtn = styled.button`
-  width: 28px;
-  height: 28px;
-  border-radius: 50%;
-  position: absolute;
-  right: 1rem;
-  cursor: pointer;
-  transition: all 0.1s ease;
-  font-weight: 500;
-  border: 1px solid #222;
-  &:hover {
-    background-color: #d2d2d2;
-  }
-`
-
-const InputTypeSection = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  width : 100%;
-`
-
-
-const ImageBtn = styled.button`
-  width: 100px;
-  height: 2.5;
-  color: #fff;
-  background-color: #272727;
-  border-radius: 20px;
-  cursor: pointer;
-  transition: all 0.5s ease;
-  border: 1px solid #eeeeee;
-  font-size: 16px;
-  font-weight: 800;
-  font-family: 'TheJamsil5Bold';
-  &:hover{
-    background-color: #171717;
-  }
-`
-
-const InputSection = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 60%;
-`
